a3b8f8578a1417d753c1e2be74647ea779a90ec2
ipromethues <to_sxl@qq.com>
2018-04-25 23:13:06 +0800

    添加Zuul测试案例

--- hello-service/src/main/java/com/fangxing/helloservice/controller/HelloController.java
+++ hello-service/src/main/java/com/fangxing/helloservice/controller/HelloController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Random;
 
 @RestController
@@ -16,12 +19,16 @@ public class HelloController {
 
     @GetMapping(value = "/hello")
     public String sayHello(){
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        System.out.println(request.getRequestURL().toString());
         return "hello world!"+client.getLocalServiceInstance().getHost();
     }
 
 
     @GetMapping(value = "/hello-delay")
     public String testDelay() throws Exception{
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        System.out.println(request.getRequestURL().toString());
         Thread.sleep(new Random().nextInt(5000));
         return "hello delay";
     }
